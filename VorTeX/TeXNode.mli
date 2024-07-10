4a5114285d6264d942fa1dfb0d4ed4166958788d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-07-10 20:57:37 +0330

    Added: Several new nodes to TeXNode.mli, but they are subject to change

--- VorTeX/TeXNode.mli
+++ VorTeX/TeXNode.mli
@@ -13,13 +13,13 @@ module TeXNode = struct
                 | GLONormal
                 | GLOFil
                 | GLOFill
-                | FLOFilll
+                | GLOFilll
         and glue_sign = 
                 | GLSStretching
                 | GLSShrinking
                 | GLSENormal
-        type node_list = texnode list * list_attr
-        type node_type = 
+        type node_list = tex_node list * list_attr
+        and tex_node = 
                 | NODERule of tex_rule
                 | NODEInsert  of tex_insert
                 | NODEMark of tex_mark
@@ -29,6 +29,10 @@ module TeXNode = struct
                 | NODEWhatsit of tex_whatsit
                 | NODEMath of tex_math
                 | NODEGlue of tex_glue
+                | NODKern of tex_kern
+                | NODEPenalty of tex_penalty
+                | NODEUnset of tex_unset
+                | NODEStyle of tex_style
         and tex_rule = (* TODO *)
         and tex_insert = (* TODO *)
         and tex_mark = (* TODO *)
@@ -37,7 +41,20 @@ module TeXNode = struct
         and tex_discretionary = (* TODO *)
         and tex_whatsit = (* TODO *)
         and tex_math = (* TODO *)
-        and tex_glue = (* TODO *)
+        and tex_glue =
+                { 
+                  stretch: TeXMetrics.elasticity; 
+                  shrink: TexMetrics.elasticity;
+                }
+        and tex_kern = 
+                | KERNExplicit of int
+                | KERNAccent of int
+        and tex_penalty =
+                | PENALTYEject of TexMetrics.neg_inf_badness
+                | PENALTYValue of int
+        and tex_unset = 
+            { glue_stretch: int; glue_shrink: int; span_count: int; }
+        and tex_style = (* TODO *)
 
 end
 
